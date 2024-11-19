fd47e355a23bc6503fb4375c5ba5e5fef79e2ed7
ThibaultLesecquePXL <thibault.lesecque@student.pxl.be>
2024-11-19 10:28:52 +0100

    Deel 1 + 2 - Versie 1.0

--- Labo-11---Arrays/MainWindow.xaml.cs
+++ Labo-11---Arrays/MainWindow.xaml.cs
@@ -16,9 +16,36 @@ namespace Labo_11___Arrays
     /// </summary>
     public partial class MainWindow : Window
     {
+        int[] numbers = new int[6] { 100, 50, 20, 60, 90, 80 };
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void outputButton_Click(object sender, RoutedEventArgs e)
+        {
+            int[] newArray = ReturnSmallestAndBiggest(numbers);
+            outputTextBox.Text = $"In totaal zijn er 6 getallen: \r\nKleinste getal = {newArray[0]}\r\nGrootste getal = {newArray[1]}";
+        }
+
+        private int[] ReturnSmallestAndBiggest(int[] numbers)
+        {
+            int[] output = new int[2] {int.MaxValue, int.MinValue};
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < output[0])
+                {
+                    output[0] = numbers[i];
+                }
+                if (numbers[i] > output[1])
+                {
+                    output[1] = numbers[i];
+                }
+            }
+
+            return output;
         }
     }
 }
